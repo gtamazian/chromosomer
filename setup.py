527fee9ab4cde61bf285dde50d0e71e42b1f2968
Gaik Tamazian <gaik.tamazian@gmail.com>
2015-07-28 17:34:53 +0300

    Setup configuration added

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015 by Gaik Tamazian
+# gaik (dot) tamazian (at) gmail (dot) com
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015 by Gaik Tamazian
+# gaik (dot) tamazian (at) gmail (dot) com
+
+from setuptools import setup, find_packages
+
+setup(name='chromosomer',
+      version='0.1.1',
+      description='Reference-assisted chromosome assembly tool',
+
+      author='Gaik Tamazian',
+      author_email='gaik.tamazian@gmail.com',
+
+      license='MIT',
+
+      classifiers=[
+          "Development Status :: 2 - Pre-Alpha",
+          "License :: OSI Approved :: MIT License",
+          "Environment :: Console",
+          "Intended Audience :: Science/Research",
+          "Natural Language :: English",
+          "Operating System :: Unix",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Scientific/Engineering :: Bio-Informatics"
+      ],
+
+      packages=find_packages(exclude=['doc', 'tests*']),
+
+      install_requires=['pyfaidx',
+                        'future'],
+
+      entry_points={
+          'console_scripts': [
+              'chromosomer = chromosomer.cli:chromosomer']
+      },
+      )
